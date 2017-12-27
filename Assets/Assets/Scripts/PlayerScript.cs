9a0192f48b288cb6a8605c15db563442aac76ba7
DKCoffee <amk1295@gmail.com>
2017-12-27 23:18:06 +0100

    Add Player move

---
+++ Assets/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    [SerializeField] float speed;
+
+    private Rigidbody2D body;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal * speed,
+                                       moveVertical * speed);
+
+        body.velocity = movement;
+    }
+}
