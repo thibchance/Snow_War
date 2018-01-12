457e68bda11727aa324b092ed04cf6034e480c51
Didtoty <Didtotysama@gmail.com>
2018-01-12 17:47:35 +0100

    voici les scenes des menus

---
+++ Assets/Scripts/Buttondirection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttondirection : MonoBehaviour {
+     public void LoadIndex (int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
+
+	
