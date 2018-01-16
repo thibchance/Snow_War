65b8860441e988f5718d693b2a753736f1759733
DKCoffee <amk1295@gmail.com>
2018-01-16 15:56:24 +0100

    Correct mouse position and camera set up

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(0, player.position.y,player.position.z -10);
+	}
+}
--- Assets/Scripts/MousePosScript.cs
+++ Assets/Scripts/MousePosScript.cs
@@ -9,14 +9,18 @@ public class MousePosScript : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        Cursor.visible = false;
+        //Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Vector3 rotation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        rotation.z = 0;
-        transform.right = rotation;
+        //Vector3 rotation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //rotation.z = 0;
+        //transform.right = rotation;
+
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
