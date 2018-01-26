e6f4b87410583b45446cde9307315a27e2c711b0
DKCoffee <amk1295@gmail.com>
2018-01-26 16:17:13 +0100

    Working on Enemy Spawn System

---
+++ Assets/Scripts/SpawnSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSystem : MonoBehaviour {
+
+    int Credits = 100;
+
+    bool startSpawn = true;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RandomEnemy()
+    {
+        int EnemySpawnchance = Random.Range(0, 101);
+
+        if(Credits <= 0)
+        {
+            startSpawn = false;
+        }
+
+        if(EnemySpawnchance<=70)
+        {
+            Debug.Log("Boy I Choose you !");
+            Credits = Credits - 10;
+        }
+        else
+        {
+            Debug.Log("Dog I Choose you !");
+            Credits = Credits - 5;
+        }
+    }
+}
