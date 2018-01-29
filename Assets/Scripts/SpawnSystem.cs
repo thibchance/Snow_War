a68df4685b9587ca2d5ed88446115196fdd1558e
DKCoffee <amk1295@gmail.com>
2018-01-29 17:30:01 +0100

    Working on Enemy Spawn System (Random spawn points)

--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -8,34 +8,53 @@ public class SpawnSystem : MonoBehaviour {
 
     bool startSpawn = true;
 
+    [SerializeField] Transform[] spawns;
+    [SerializeField] GameObject boy;
+    [SerializeField] GameObject dog;
+
+
     // Use this for initialization
-    void Start () {
-		
+    void Start ()
+    {
+        StartCoroutine("RandomSpawn");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Debug.Log("Credits" + Credits);
 	}
+
+    IEnumerator RandomSpawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            RandomEnemy();
+            if (Credits <= 0)
+            {
+                yield return null;
+            }
+        }
+    }
 
     public void RandomEnemy()
     {
         int EnemySpawnchance = Random.Range(0, 101);
 
-        if(Credits <= 0)
-        {
-            startSpawn = false;
-        }
-
         if(EnemySpawnchance<=70)
         {
             Debug.Log("Boy I Choose you !");
             Credits = Credits - 10;
+            int spawnIndex = Random.Range(0, spawns.Length);
+            Instantiate(boy, spawns[spawnIndex].position, spawns[spawnIndex].rotation);
         }
         else
         {
             Debug.Log("Dog I Choose you !");
             Credits = Credits - 5;
+            int spawnIndex = Random.Range(0, spawns.Length);
+            Instantiate(dog, spawns[spawnIndex].position, spawns[spawnIndex].rotation);
         }
     }
+
 }
